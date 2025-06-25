1f17c32a52d01a6909518471e37aa3e3e89254cc
Lucas Batista <batistalucasprof@gmail.com>
2025-06-24 23:22:24 -0300

    2025-06-24

--- avaliacao-01/SistemaEscolar.java
+++ avaliacao-01/SistemaEscolar.java
@@ -2,39 +2,50 @@ import java.util.Scanner;
 
 public class SistemaEscolar {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double[] notas = new double[8];
+        Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Digite as 8 notas anuais:");
+        // Entrada das 8 notas
+        System.out.println("Digite as 8 notas do ano:");
+        System.out.print("Nota 1: ");
+        double nota1 = entrada.nextDouble();
+        System.out.print("Nota 2: ");
+        double nota2 = entrada.nextDouble();
+        System.out.print("Nota 3: ");
+        double nota3 = entrada.nextDouble();
+        System.out.print("Nota 4: ");
+        double nota4 = entrada.nextDouble();
+        System.out.print("Nota 5: ");
+        double nota5 = entrada.nextDouble();
+        System.out.print("Nota 6: ");
+        double nota6 = entrada.nextDouble();
+        System.out.print("Nota 7: ");
+        double nota7 = entrada.nextDouble();
+        System.out.print("Nota 8: ");
+        double nota8 = entrada.nextDouble();
 
-        for (int i = 0; i < 8; i++) {
-            System.out.print("Nota " + (i + 1) + ": ");
-            notas[i] = scanner.nextDouble();
-        }
+        // Cálculos
+        double bimestre1 = (nota1 + nota2) / 2;
+        double bimestre2 = (nota3 + nota4) / 2;
+        double semestre1 = (bimestre1 + bimestre2) / 2;
 
-        // Cálculos dos bimestres
-        double b1 = (notas[0] + notas[1]) / 2;
-        double b2 = (notas[2] + notas[3]) / 2;
-        double s1 = (b1 + b2) / 2;
+        double bimestre3 = (nota5 + nota6) / 2;
+        double bimestre4 = (nota7 + nota8) / 2;
+        double semestre2 = (bimestre3 + bimestre4) / 2;
 
-        double b3 = (notas[4] + notas[5]) / 2;
-        double b4 = (notas[6] + notas[7]) / 2;
-        double s2 = (b3 + b4) / 2;
+        double mediaFinal = (semestre1 + semestre2) / 2;
 
-        double mediaFinal = (s1 + s2) / 2;
-
-        // Exibição dos resultados
+        // Saída
         System.out.println("\nResultados:");
-        System.out.println("1º Bimestre: " + b1);
-        System.out.println("2º Bimestre: " + b2);
-        System.out.println("1º Semestre: " + s1);
+        System.out.println("1º Bimestre: " + bimestre1);
+        System.out.println("2º Bimestre: " + bimestre2);
+        System.out.println("1º Semestre: " + semestre1);
         System.out.println("----------------------");
-        System.out.println("3º Bimestre: " + b3);
-        System.out.println("4º Bimestre: " + b4);
-        System.out.println("2º Semestre: " + s2);
+        System.out.println("3º Bimestre: " + bimestre3);
+        System.out.println("4º Bimestre: " + bimestre4);
+        System.out.println("2º Semestre: " + semestre2);
         System.out.println("----------------------");
         System.out.println("Média Final: " + mediaFinal);
 
-        scanner.close();
+        entrada.close();
     }
 }
